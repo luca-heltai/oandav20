67320f2f55d103263e2032fe9a60d423dc619cc5
nait-naul <nait-aul@protonmail.com>
2016-08-15 14:52:24 +0200

    Prepared test class for the OrdersMixin.

---
+++ tests/test_orders_endpoints.py
@@ -0,0 +1,12 @@
+import unittest
+
+from requests import HTTPError
+
+from oandav20.testing improt TestCase
+
+
+class TestOrdersMethods(TestCase):
+    """Tests for the OrdersMixin class."""
+
+    def create_market_order(self):
+        pass
